180827737126dd258d8c42a647c22052ecf321e4
licetrosales <licetrosales@gmail.com>
2023-02-24 14:27:04 +0100

    Create serverto listen on port 5000

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ router.get('/', (req, res, next) => {
     res.send("Apple");
 });
 app.use('/api/', router);
+var server = app. listen(500, () =>{
+   console.log("Node server is running on http://localhost:5000...");
+});
 
 
